266e773a7966e918d981b21796db7b73fbde2e83
xf <lxufeng90@163.com>
2019-08-14 09:11:19 +0800

    add

--- get_data/getDataFromXinLang.py
+++ get_data/getDataFromXinLang.py
@@ -9,5 +9,12 @@ from lxml import etree
 
 data = requests.get(url="http://vip.stock.finance.sina.com.cn/quotes_service/view/vMS_tradedetail.php?symbol=sz002405&date=2019-07-31&page=60").content.decode("gbk")
 data0 = etree.HTML(data)
-result = data0.xpath('//*[@id="datatbl"]')
-print(result)
+result = data0.xpath('//*[@id="datatbl"]/tbody//*')  # //后面跟所有的子元素
+print(len(result))
+for r in result:
+    print(r.tag, r.text)
+
+data = '%'.join([res.tag + '=' + res.text for res in result])
+splitData = data.split("tr=None")
+for sd in splitData:
+    print(sd.split('%'))
