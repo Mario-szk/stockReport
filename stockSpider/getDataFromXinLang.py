038853c911972334c39867b3fc4ed51ef68c5ecb
lixufeng <lixufeng@zhuanzhuan.com>
2019-08-14 13:10:38 +0800

    add

--- stockSpider/getDataFromXinLang.py
+++ stockSpider/getDataFromXinLang.py
@@ -9,5 +9,8 @@ from lxml import etree
 
 data = requests.get(url="http://vip.stock.finance.sina.com.cn/quotes_service/view/vMS_tradedetail.php?symbol=sz002405&date=2019-07-31&page=60").content.decode("gbk")
 data0 = etree.HTML(data)
-result = data0.xpath('//*[@id="datatbl"]')
-print(result)
+result = data0.xpath('//*[@id="datatbl"]/tbody/tr[1]/*')
+print(len(result))
+for r in result:
+    print(r.tag, r.text)
+
