3545da6fa3f52aa3a2afd973d0d2b359cc6844a5
lixufeng <lixufeng@zhuanzhuan.com>
2019-08-19 10:00:41 +0800

    data to dataframe demo

--- stockSpider/getDataFromXinLang.py
+++ stockSpider/getDataFromXinLang.py
@@ -6,15 +6,25 @@
 
 import requests
 from lxml import etree
-
-data = requests.get(url="http://vip.stock.finance.sina.com.cn/quotes_service/view/vMS_tradedetail.php?symbol=sz002405&date=2019-07-31&page=60").content.decode("gbk")
+import pandas as pd
+import numpy as np
+"""
+数据处理部分
+"""
+# 获取基础数据
+data = requests.get(url="http://vip.stock.finance.sina.com.cn/quotes_service/view/vMS_tradedetail.php?symbol=sz002405&date=2019-08-14&page=11").content.decode("gbk")
 data0 = etree.HTML(data)
 result = data0.xpath('//*[@id="datatbl"]/tbody//*')  # //后面跟所有的子元素
 print(len(result))
 for r in result:
     print(r.tag, r.text)
 
-data = '%'.join([res.tag + '=' + res.text for res in result])
+# 数据整理
+data = '%'.join([str(res.tag) + '=' + str(res.text) for res in result])
 splitData = data.split("tr=None")
-for sd in splitData:
-    print(sd.split('%'))
+
+splitData = [a.split('%') for a in splitData]
+# 数据转至dataframe
+
+dataPd = pd.DataFrame(splitData,columns=[1,2,3,4,5,6,7,8,9,10,11])
+print(dataPd)
